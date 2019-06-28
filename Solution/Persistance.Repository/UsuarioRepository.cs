7bdfb3ff2d7edf9b37de8e11987b806f54e790c1
yibambe <51690440+yibambe@users.noreply.github.com>
2019-06-27 23:44:23 -0500

    3.2

---
+++ Solution/Persistance.Repository/UsuarioRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NLog;
+using Persistance.DatabaseContext;
+
+namespace Persistance.Repository
+{
+    public interface IUsuario
+    {
+        Usuario GetById(Usuario usuario);
+    }
+    class UsuarioRepository : IUsuario
+    {
+        public Usuario GetById(Usuario usuario)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                
+
+            }
+            return null;
+        }
+    }
+}

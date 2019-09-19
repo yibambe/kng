18e62d0510566996eebd893df68017762851f547
yibambe <51690440+yibambe@users.noreply.github.com>
2019-09-19 11:30:32 -0500

    7

--- Solution/Service/App_Start/UnityConfig.cs
+++ Solution/Service/App_Start/UnityConfig.cs
@@ -1,7 +1,7 @@
 using System.Web.Http;
 using Unity;
 using Unity.WebApi;
-using Persistance.KRepository;
+
 namespace Service
 {
     public static class UnityConfig
@@ -9,15 +9,12 @@ namespace Service
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-
+            
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-
+            
             // e.g. container.RegisterType<ITestService, TestService>();
-
-            // start
-            container.RegisterType<IUsuario, UsuarioRepository>();
-            // end
+            
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
